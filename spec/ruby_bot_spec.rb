7afa8d75f5592ee92bacbd17be91d15caf4fd1b6
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-09 23:02:10 -0600

    Add test suite

---
+++ spec/ruby_bot_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/ruby_bot'
+
+describe Rubybot do
+  describe '#initialize' do
+    it 'generates new client object' do
+      bot = Rubybot.new
+      expect(bot.client.is_a?(Twitter::REST::Client)).to be(true)
+    end
+  end
+end
