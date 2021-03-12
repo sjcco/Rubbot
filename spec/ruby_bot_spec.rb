3ef4a34cd54573f17a9a730d4417e93ba9a57999
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-12 13:00:44 -0600

    Add spec for post_tweet and get_message

--- spec/ruby_bot_spec.rb
+++ spec/ruby_bot_spec.rb
@@ -8,4 +8,31 @@ describe Rubybot do
       expect(bot.client.is_a?(Twitter::REST::Client)).to be(true)
     end
   end
+
+  describe '#post_tweet' do
+    bot = Rubybot.new
+    it 'Returns tweet object' do
+      expect(bot.post_tweet(rand(1..1000).to_s).class).to be(Twitter::Tweet)
+    end
+  end
+
+  describe '#get_message' do
+    bot = Rubybot.new
+    it 'Returns hello' do
+      new_tweet = bot.post_tweet("#{rand(1..1000)} #hello")
+      expect(bot.get_message(new_tweet)).to eql('#Hello to you too')
+    end
+    it 'Returns I\'m also a robot' do
+      new_tweet = bot.post_tweet("#{rand(1..1000)} #iamarobot")
+      expect(bot.get_message(new_tweet)).to eql('I\'m also a Robot')
+    end
+    it 'Returns I\'ll follow' do
+      new_tweet = bot.post_tweet("#{rand(1..1000)} #followme")
+      expect(bot.get_message(new_tweet)).to eql('Yes I\'ll follow you')
+    end
+    it 'Returns do not reply' do
+      new_tweet = bot.post_tweet("#{rand(1..1000)}")
+      expect(bot.get_message(new_tweet)).to eql('do not reply')
+    end
+  end
 end
