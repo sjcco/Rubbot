216963afaedae691a5cf00bac279df44a3798d93
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-12 13:10:07 -0600

    Add store id to rspec

--- spec/ruby_bot_spec.rb
+++ spec/ruby_bot_spec.rb
@@ -18,20 +18,29 @@ describe Rubybot do
 
   describe '#get_message' do
     bot = Rubybot.new
+    def store_id(mention)
+      file = File.open('stored_ids.txt', mode: 'w')
+      file.write(mention.id.to_s)
+      file.close
+    end
     it 'Returns hello' do
       new_tweet = bot.post_tweet("#{rand(1..1000)} #hello")
+      store_id(new_tweet)
       expect(bot.get_message(new_tweet)).to eql('#Hello to you too')
     end
     it 'Returns I\'m also a robot' do
       new_tweet = bot.post_tweet("#{rand(1..1000)} #iamarobot")
+      store_id(new_tweet)
       expect(bot.get_message(new_tweet)).to eql('I\'m also a Robot')
     end
     it 'Returns I\'ll follow' do
       new_tweet = bot.post_tweet("#{rand(1..1000)} #followme")
+      store_id(new_tweet)
       expect(bot.get_message(new_tweet)).to eql('Yes I\'ll follow you')
     end
     it 'Returns do not reply' do
       new_tweet = bot.post_tweet("#{rand(1..1000)} dont reply")
+      store_id(new_tweet)
       expect(bot.get_message(new_tweet)).to eql('do not reply')
     end
   end
